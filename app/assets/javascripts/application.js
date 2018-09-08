3fc239bf6f4b82781bfd71957a658df0116dfe8a
Koichiro Taoka <koichirotaoka@gmail.com>
2018-09-08 10:06:27 +0000

    マダガスカル

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,5 +15,7 @@
 //
 //= require rails-ujs
 //= require turbolinks
+//= require jquery/dist/jquery.js
+//= require bootstrap/dist/js/bootstrap.min
 //= require_tree .
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
-  root 'blogs#index'
+  root 'blogs#show'
 end
