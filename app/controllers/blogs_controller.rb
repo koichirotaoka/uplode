f86019352fe4b558cb4ec92bcfb3645a9e996636
Koichiro Taoka <koichirotaoka@gmail.com>
2018-10-31 12:56:45 +0000

    workers Integer(ENV['WEB_CONCURRENCY'] || 2)

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -23,7 +23,6 @@ class BlogsController < ApplicationController
     @blog = Blog.new(blog_params)
     @blog.user_id = current_user.id 
     if @blog.save
-      BlogMailer.blog_mail(@blog).deliver 
       redirect_to blogs_path, notice: 'ブログを作成しました！'
     else
       render 'new'
