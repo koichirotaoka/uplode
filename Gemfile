828ee3dee6f01b2913e1467868abaa335dbb613e
Koichiro Taoka <koichirotaoka@gmail.com>
2018-11-01 03:44:26 +0000

    icon完成

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'mini_magick', '~> 4.5', '>= 4.5.1'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.1.1'
+gem 'rails', '~> 5.1.6'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.18'
 # Use Puma as the app server
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181029105337) do
+ActiveRecord::Schema.define(version: 20181101030030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 20181029105337) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "image"
+    t.text "icon"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
